5ace2e204b3d1818ed69644c9e451928bd63b97a
athanasdev <benjaminiathanas@gmail.com>
2025-09-17 05:25:25 +0300

    updated css

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO: Improve UI in style.css
+
+- [x] Replace all instances of "yellow" with "#FFD700" (gold color)
+- [x] Enhance box-shadows for a more modern look
+- [x] Increase border-radius for rounded corners
+- [x] Improve hover effects with smoother transitions
+- [ ] Adjust color contrasts for better accessibility
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -36,8 +36,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{asset($site_image->logo)}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="#" class="brand-link">
+      <img src="{{asset($site_image->logo)}}" alt="Admin Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">{{$site_info->site_name ?? ''}}</span>
     </a>
 
